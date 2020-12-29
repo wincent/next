b360877f10c03c002360243264b5e8efab8aae26
Greg Hurrell <greg@hurrell.net>
2020-12-29 18:31:40 +0100

    refactor: apply neater fix for Node v10 failures on CI

--- babel.config.js
+++ babel.config.js
@@ -4,9 +4,8 @@
  */
 
 module.exports = {
-  plugins: ['@babel/plugin-proposal-class-properties'],
   presets: [
-    ['@babel/preset-env', {targets: {node: 'current'}}],
+    ['@babel/preset-env', {targets: {node: '10'}}],
     '@babel/preset-typescript',
   ],
 };
