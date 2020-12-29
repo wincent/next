4c2061d00d3f83a8c250e9381bdc2b0cf428af3a
Greg Hurrell <greg@hurrell.net>
2020-12-29 19:02:27 +0100

    test: revert to previous fix
    Sigh; the uglier fix is apparently still necessary:
    
        https://github.com/wincent/next/runs/1622754433
    
    This reverts commit b360877f10c03c002360243264b5e8efab8aae26.

--- babel.config.js
+++ babel.config.js
@@ -4,8 +4,9 @@
  */
 
 module.exports = {
+  plugins: ['@babel/plugin-proposal-class-properties'],
   presets: [
-    ['@babel/preset-env', {targets: {node: '10'}}],
+    ['@babel/preset-env', {targets: {node: 'current'}}],
     '@babel/preset-typescript',
   ],
 };
