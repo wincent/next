9b3a8ec3ba600274dd7097e4fa727fd60e2f49f7
Greg Hurrell <greg@hurrell.net>
2020-12-29 18:26:06 +0100

    test: attempt fix for CI failure on Node v10
    Example failure here:
    
        https://github.com/wincent/next/runs/1622716501

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@
  */
 
 module.exports = {
+  plugins: ['@babel/plugin-proposal-class-properties'],
   presets: [
     ['@babel/preset-env', {targets: {node: 'current'}}],
     '@babel/preset-typescript',
