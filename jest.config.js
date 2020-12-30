7a333a270fe385d70c9c1866fabc483ab789f3f5
Greg Hurrell <greg@hurrell.net>
2020-12-30 12:41:21 +0100

    test: make sure jest doesn't run twice
    It was running on source files in "src"/ and also on built files in
    "lib/", which could yield the confusing situation of seeing "src/" tests
    pass (because they've been fixed) but "lib/" tests fail (because you
    haven't run `yarn build` yet). This is particularly confusing if running
    the tests with `yarn test --watch`, because it drives up the odds of
    running the tests before you've done a build to near 100%.

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/**
+ * SPDX-FileCopyrightText: Copyright 2020 Greg Hurrell
+ * SPDX-License-Identifier: MIT
+ */
+
+module.exports = {
+  testPathIgnorePatterns: ['<rootDir>/lib/', '<rootDir>/node_modules/'],
+};
